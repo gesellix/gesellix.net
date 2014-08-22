50b4254e30be2e463893a853cacefb8d08d189af
Tobias Gesellchen <tobias@gesellix.de>
2014-08-22 16:54:56 +0200

    literary-material (ghost 0.5.0 compatible) from http://literary-material.launchpage.me/

--- ghost/content/themes/literary-material/assets/js/index.js
+++ ghost/content/themes/literary-material/assets/js/index.js
@@ -4,29 +4,29 @@
 
 /*globals jQuery, document */
 (function ($) {
-    "use strict";
+  "use strict";
 
-    $(document).ready(function(){
+  $(document).ready(function(){
 
-        $(".post-content").fitVids();
+    $(".post-content").fitVids();
 
-        function casperFullImg() {
-            $("img").each( function() {
-                var contentWidth = $(".post-content").outerWidth(); // Width of the content
-                var imageWidth = $(this)[0].naturalWidth; // Original image resolution
+    function casperFullImg() {
+      $("img").each( function() {
+        var contentWidth = $(".post").outerWidth(); // Width of the content
+        var imageWidth = $(this)[0].naturalWidth; // Original image resolution
 
-                if (imageWidth >= contentWidth) {
-                    $(this).addClass('full-img');
-                } else {
-                    $(this).removeClass('full-img');
-                }
-            });
-        };
+        if (imageWidth >= contentWidth) {
+          $(this).addClass('full-img');
+        } else {
+          $(this).removeClass('full-img');
+        }
+      });
+    };
 
-        casperFullImg();
-        $(window).smartresize(casperFullImg);
+    casperFullImg();
+    $(window).smartresize(casperFullImg);
 
-    });
+  });
 
 }(jQuery));
 
@@ -35,23 +35,23 @@
   // debouncing function from John Hann
   // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
   var debounce = function (func, threshold, execAsap) {
-      var timeout;
+    var timeout;
 
-      return function debounced () {
-          var obj = this, args = arguments;
-          function delayed () {
-              if (!execAsap)
-                  func.apply(obj, args);
-              timeout = null;
-          };
-
-          if (timeout)
-              clearTimeout(timeout);
-          else if (execAsap)
-              func.apply(obj, args);
-
-          timeout = setTimeout(delayed, threshold || 100);
+    return function debounced () {
+      var obj = this, args = arguments;
+      function delayed () {
+        if (!execAsap)
+            func.apply(obj, args);
+        timeout = null;
       };
+
+      if (timeout)
+        clearTimeout(timeout);
+      else if (execAsap)
+        func.apply(obj, args);
+
+      timeout = setTimeout(delayed, threshold || 100);
+    };
   }
   // smartresize
   jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
--- ghost/content/themes/literary-material/assets/js/materiality.js
+++ ghost/content/themes/literary-material/assets/js/materiality.js
@@ -1,30 +1,104 @@
 (function($) {
 
+  // PREPARE VIEW
+  var $spacer = $("#header-spacer");
+
+  // resize header spacer
+  var resizeHeaderSpacer = function($spacer) {
+    $spacer.height($(".site-head").height());
+  }
+  resizeHeaderSpacer($spacer);
+
+  // ON RESIZE
+
+  // resize header spacer
+  $(window).resize(function() {
+    resizeHeaderSpacer($spacer);
+  });
+
+  // RESIZE IMAGES
+
+  // taken from index.js
+  function casperFullImg() {
+    $("img:not(footer img)").each( function() {
+      // debugger;
+      var contentWidth = $(".post").outerWidth(); // Width of the content
+      var imageWidth = $(this)[0].naturalWidth; // Original image resolution
+
+      if (imageWidth >= contentWidth) {
+        $(this).addClass('full-img');
+      } else {
+        $(this).removeClass('full-img');
+      }
+    });
+  };
+
+  var $container = $(".content");
+  if ($container.length > 0) {
+    $container.imagesLoaded( function() {
+      casperFullImg();
+    });
+  }
+
+  // SCROLL ACTIONS
+
   navigation_height = 100;
   navigation_min_height = 0;
 
-  $(window).scroll(function() {
-    var $header = $("header.post-header");
-    var $spacer = $("#header-spacer");
+  var originalHeaderHeight = $(".site-head").height();
+  var shrinkOrExpandHeader = function($header, $spacer) {
+    // big screen
+    if ($header !== undefined && $(window).width() > 500) {
 
+      if ( $(document).scrollTop() > originalHeaderHeight - 51) {
+        if ($header.hasClass("small-header") == false) {
+          // console.log("ADD CLASS - BIG");
+          $header.addClass("small-header");
+          // $spacer.addClass("small-header");
+        }
+      }
+
+      if ($(document).scrollTop() < originalHeaderHeight - 51) {
+        if ($header.hasClass("small-header") == true) {
+          // console.log("REMOVE CLASS - BIG");
+          $header.removeClass("small-header");
+          // $spacer.removeClass("small-header");
+        }
+      }
+
+    // small screen
+    } else if ($header !== undefined && $(window).width() < 500) {
+
+      if ( $(document).scrollTop() > originalHeaderHeight - 51) {
+        if ($header.hasClass("small-header") == false) {
+          // console.log("ADD CLASS - BIG");
+          $header.addClass("small-header");
+          // $spacer.addClass("small-header");
+        }
+      }
+
+      if ($(document).scrollTop() < originalHeaderHeight - 51) {
+        if ($header.hasClass("small-header") == true) {
+          // console.log("REMOVE CLASS - BIG");
+          $header.removeClass("small-header");
+          // $spacer.removeClass("small-header");
+        }
+      }
+
+    }
+  }
+
+  $(window).scroll(function() {
     // post header
-    if ($(document).scrollTop() > 50) {
-      $header.addClass("small-header");
-      $spacer.addClass("small-header");
-    } else {
-      $header.removeClass("small-header");
-      $spacer.removeClass("small-header");
+    var $header = $("header.post-header");
+    if ($header !== undefined) {
+      shrinkOrExpandHeader($header, $spacer);
     }
 
     // homepage header
     var $header = $("header.site-head");
-
-    if ($(document).scrollTop() > 50) {
-      $header.addClass("small-header");
-      $spacer.addClass("small-header");
-    } else {
-      $header.removeClass("small-header");
-      $spacer.removeClass("small-header");
+    if ($header !== undefined) {
+      shrinkOrExpandHeader($header, $spacer);
     }
 
     // scroll top
@@ -37,7 +111,7 @@
 
   // transitions and animations
 
-  $(".content article").velocity("transition.slideLeftIn", { stagger: 250 }, { duration: 1500 });
+  $(".content article").velocity("transition.swoopIn", { stagger: 250 }, { duration: 1500 });
 
   $(".tag-archive-header").velocity("transition.slideUpIn", { stagger: 250 }, { duration: 1500 });
 
@@ -69,6 +143,39 @@
 
   $("#scroll-top").click(function(){
     $("html, body").animate({ scrollTop: 0 }, "slow");
+  });
+
+  // SIMPLE BOX
+  // make your own lightbox cuz they all suck
+
+  var imgSrc
+    , $simpleBox;
+
+  window.fitImg = function() {
+    var $img = $("#simple-box img");
+    $($img).css("max-width", $(window).width() - 20);
+    $($img).css("max-height", $(window).height() - 40);
+  };
+
+  $("body").on("click", ".post img", function(e) {
+    // prevent same event from firing twice
+    // if (isJqmGhostClick(e)) { return; }
+
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+      // NO OP
+      // Sorry, no image modal on mobile, blame Google's mobile Chrome browser for being so shitty...
+    } else {
+      $("#simple-box").remove(); // remove if one already exists
+      imgSrc = $(e.target).attr("src");
+      $simpleBox = $("<div id='simple-box' style='cursor:pointer;cursor:-webkit-zoom-out;cursor:-moz-zoom-out;cursor:zoom-out;display:table;position:fixed;width:100%;height:calc(100% + 160px);height:calc(100vh + 160px);margin-top:-80px;z-index:999999;top:0;left:0;right:0;bottom:0;'><div style='background-color:rgba(0,0,0,0.7);display:table-cell;vertical-align:middle;text-align:center;top:0;bottom:0;left:0;right:0;width:100%;height:100%;'><img class='fade-in one fast' src='"+ imgSrc +"' style='box-shadow:0 0 50px rgba(0,0,0,0.5);' onload='fitImg()'></div></div>");
+      $("body").append($simpleBox);
+    }
+
+  });
+
+  // "close" the modal
+  $("body").on("click", "#simple-box", function(e) {
+    $("#simple-box").remove();
   });
 
 }(jQuery));
